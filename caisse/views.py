b563e7be37fa4633be070068e19a01e8c994bb71
kems205 <kemulebezara205@gmail.com>
2024-10-15 10:32:08 +0300

    Modification des noms des templates pour ne pas avoir de confusion

--- caisse/views.py
+++ caisse/views.py
@@ -5,20 +5,20 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required  # Ajout du décorateur pour restreindre l'accès
 def index(request):
-    return render(request, "caisse/index.html")
+    return render(request, "caisse/dashboard.html")
 
 @login_required
 def operations(request):
-    return render(request, "caisse/operations/index.html")  # Créez ce fichier HTML
+    return render(request, "caisse/operations/entre-sortie.html")  # Créez ce fichier HTML
 
 @login_required
 def listes(request):
-    return render(request, "caisse/listes/index.html")  # Créez ce fichier HTML
+    return render(request, "caisse/listes/listes_operations.html")  # Créez ce fichier HTML
 
 @login_required
 def depenses(request):
-    return render(request, "caisse/depenses/index.html")  # Créez ce fichier HTML
+    return render(request, "caisse/depenses/depense.html")  # Créez ce fichier HTML
 
 @login_required
 def acteurs(request):
-    return render(request, "caisse/acteurs/index.html")  # Créez ce fichier HTML
+    return render(request, "caisse/acteurs/acteurs.html")  # Créez ce fichier HTML
