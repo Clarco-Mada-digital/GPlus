b2fbe39fd6eece1a61f150fff3d4bb0ffd5201e9
kems205 <kemulebezara205@gmail.com>
2024-10-31 10:13:34 +0300

    Misy remplacement nataoko t@url mila misy app_name

--- GPP/settings.py
+++ GPP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xx3-u&#hmg3ylrp5a05gyh+31p&#@!h@-mf8_dbr*$7gc)3oek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
