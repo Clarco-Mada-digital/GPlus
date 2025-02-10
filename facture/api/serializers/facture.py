f8ef536ff46a8e4a1304c0ab295bfd5b7f952865
malcovys <malcovys22.aps@gmail.com>
2025-02-10 14:00:22 +0300

    generation ref

--- facture/api/serializers/facture.py
+++ facture/api/serializers/facture.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.db import transaction
 from rest_framework import serializers
 
 from facture.models import Facture
@@ -27,27 +28,44 @@ class FactureListSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['ref', 'created_by'] # Empêche la modification de ces champs
 
-    def create(self, validated_data): # Surcharge de la méthode create pour générer la référence de la facture
-        validated_data['ref'] = self.generate_ref(validated_data)
+    # Surcharge de la méthode create pour générer la référence de la facture
+    @transaction.atomic
+    def create(self, validated_data):
+        # Générer une référence provisoire
+        validated_data['ref'] = f"TEMP_REF-{str(timezone.now().date())}-{str(timezone.now().time())}"
+
         validated_data['created_by'] = self.context['request'].user
-        return super().create(validated_data)
-
-    def generate_ref(self, data): # Génère la référence de la facture
-        facture_year = str(timezone.now().year) # Récupère l'année actuelle
-        facture_id = Facture.objects.all().order_by('id').last().id + 1 # Récupère le dernier id de la facture et l'incrémente de 1
-        facture_ref_end = f"{facture_year}-"+str(facture_id).zfill(6) # Concatène l'année et l'id de la facture, zfill permet de compléter l'id avec des 0
         
-        if data['etat_facture'] == 'Brouillon':
-            if data['type'] == 'Facture':
-                return "FPROV"+facture_ref_end
-            elif data['type'] == 'Devis':
-                return "DPROV"+facture_ref_end
-        else :
-            if data['type'] == 'Facture':
-                return "F"+facture_ref_end
-            elif data['type'] == 'Devis':
-                return "D"+facture_ref_end
+        # Créer la facture avec une référence provisoire
+        facture = super().create(validated_data)
+        
+        # Mettre à jour la référence de la facture après l'enregistrement
+        facture.ref = self.generate_ref(facture)  # Regénérer le ref avec l'ID réel
+        facture.save()  # Sauvegarder la facture avec la référence mis à jour
+        
+        return facture
 
+    # Génère la référence de la facture
+    def generate_ref(self, facture): 
+        # Récupère l'année actuelle
+        facture_year = str(timezone.now().year)
+
+        # Récupère l'ID
+        facture_id = facture.id
+
+        # Concatène l'année et l'id de la facture, zfill permet de compléter l'id avec des 0
+        facture_ref_end = f"{facture_year}-"+str(facture_id).zfill(6)
+        
+        if facture.etat_facture == 'Brouillon':
+            if facture.type == 'Facture':
+                return "FPROV" + facture_ref_end
+            elif facture.type == 'Devis':
+                return "DPROV" + facture_ref_end
+        else:
+            if facture.type == 'Facture':
+                return "F" + facture_ref_end
+            elif facture.type == 'Devis':
+                return "D" + facture_ref_end
 
 # Utilisée pour sérialiser les détails d'une facture
 class FactureDetailSerializer(serializers.ModelSerializer):
