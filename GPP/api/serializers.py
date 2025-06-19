2a635d58f519e8a837103d37eab618e58d89ae43
malcovys <malcovys22.aps@gmail.com>
2025-06-19 15:49:49 +0300

    Ajout de l'URL de la photo utilisateur dans le serializer JWT

--- GPP/api/serializers.py
+++ GPP/api/serializers.py
@@ -8,12 +8,22 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         
         # Ajouter les informations de l'utilisateur directement dans la réponse
         user = self.user
+
+        # Construction de l'URL de la photo si elle existe
+        photo_url = None
+        if user.photo:
+            try:
+                photo_url = user.photo.url
+            except:
+                photo_url = None
+
         data['user'] = {
             'id': user.id,
             'username': user.username,
             'email': user.email,
             'first_name': user.first_name,
             'last_name': user.last_name,
+            'photo': photo_url,
         }
         
         return data
